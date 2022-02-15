f32d05afbff1639259e02da36006441aeebc4bb2
pabli <pabli@PCS-PABLO>
2022-02-15 13:48:04 +0100

    Commit inicial, Atributos y Constructor

---
+++ src/proyectoPabloLadronOrdoyo/PabloLadronCine.java
@@ -0,0 +1,17 @@
+package proyectoPabloLadronOrdoyo;
+
+public class PabloLadronCine {
+
+	//Atributos
+	public String nombreHobby;
+	public String nombreDirector;
+	public String nombreCompositor;
+	
+	//Constructor
+	public PabloLadronCine(String nombreHobby, String nombreDirector, String nombreCompositor) {
+		super();
+		this.nombreHobby = nombreHobby;
+		this.nombreDirector = nombreDirector;
+		this.nombreCompositor = nombreCompositor;
+	}
+}
